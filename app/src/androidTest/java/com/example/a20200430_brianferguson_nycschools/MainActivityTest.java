63a2f76b7bf702dfffe9fa86ef0ac433064a69bb
Brian Ferguson <brian.ferguson@mobileappscompany.us>
2020-05-01 13:58:16 -0500

    added UI tests

--- app/src/androidTest/java/com/example/a20200430_brianferguson_nycschools/MainActivityTest.java
+++ app/src/androidTest/java/com/example/a20200430_brianferguson_nycschools/MainActivityTest.java
@@ -1,25 +1,47 @@
 package com.example.a20200430_brianferguson_nycschools;
 
 import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.rule.ActivityTestRule;
 
 import com.example.a20200430_brianferguson_nycschools.view.MainActivity;
+import com.example.a20200430_brianferguson_nycschools.view.ScoresActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 public class MainActivityTest {
 
     @Rule
-    public ActivityTestRule activityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public IntentsTestRule<MainActivity> intentsTestRule = new IntentsTestRule<>(MainActivity.class);
 
     @Test
-    public void activityLaunched() {
+    public void shouldLaunchScoresActivity() throws InterruptedException {
+        Thread.sleep(5000);
         onView(withId(R.id.school_list))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+
+        intended(hasComponent(ScoresActivity.class.getName()));
+    }
+
+    @Test
+    public void shouldNotLaunchScoresActivity() throws InterruptedException {
+        Thread.sleep(5000);
+        onView(withId(R.id.school_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        onView(withText("Could not retrieve data for school")).inRoot(withDecorView(not(is(intentsTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
     }
 }
